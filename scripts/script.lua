e6d3033071b620faa5e9bb51e60a9f6e281611a0
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-26 14:32:38 +0700

    Update script.lua

--- scripts/script.lua
+++ scripts/script.lua
@@ -1,3 +1,24 @@
+function onPlace()
+end
+
 function start()
  print("hello world")
+end
+
+function update()
+end
+
+function fixedUpdate()
+end
+
+function onButtonClick()
+end
+
+function onButtonDown()
+end
+
+function onButtonUp()
+end
+
+function message(msg)
 end
