9182618f3c84e624459daa47197c469f5bb1a884
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-28 16:53:12 +0700

    Update script.lua

--- scripts/script.lua
+++ scripts/script.lua
@@ -1,9 +1,9 @@
 function onPlace()
  print("hello world")
+ es.CreateMultiblock("SoccerBall",r,{0.0,0.5,0.0},q.Euler(0.0,0.0,0.0))
 end
 
 function start()
- es.CreateMultiblock("SoccerBall",r,{0.0,0.5,0.0},q.Euler(0.0,0.0,0.0))
 end
 
 function update()
