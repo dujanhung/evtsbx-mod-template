0b87b6f50f3897c80db32cc533e46a97718f4367
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-28 17:42:11 +0700

    Create script.lua

---
+++ simpleblocks/mod_uuid/script.lua
@@ -0,0 +1,38 @@
+dofile("addons_orchestrator/ALL.lua")
+
+function onPlace()
+end
+
+function start()
+end
+
+function update()
+end
+
+function fixedUpdate()
+end
+
+function onButtonClick()
+end
+
+function onButtonDown()
+end
+
+function onButtonUp()
+end
+
+function canConnect(blockType,thisIdx,otherIdx,reverse)
+ if blockType=="DriverSeat"then
+  return false
+ end
+ if blockType=="PassengerSeat"then
+  return false
+ end
+ return true
+end
+
+function onSignalReceived(value,guid,idx)
+end
+
+function message(msg)
+end
