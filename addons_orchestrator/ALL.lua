52cdf77870ce28397ba134af2a8df47b8e764685
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-30 06:10:23 +0700

    Update ALL.lua

--- addons_orchestrator/ALL.lua
+++ addons_orchestrator/ALL.lua
@@ -1,5 +1,5 @@
-module("MOD_orchestrator")
-MOD_orchestrator.file_root="addons/"
-MOD_orchestrator.presets={"extra-lua-preset/","evtsbx-mod-lua-preset/"}
-MOD_orchestrator.script_file="orchestrator/ALL.lua"
-for i in MOD_orchestrator.presets do dofile(MOD_orchestrator.file_root..i..MOD_orchestrator.script_file)end
+module("addons_orchestrator")
+addons_orchestrator.file_root="addons/"
+addons_orchestrator.presets={"extra-lua-preset/","evtsbx-mod-lua-preset/"}
+addons_orchestrator.script_file="orchestrator/ALL.lua"
+for i in addons_orchestrator.presets do dofile(addons_orchestrator.file_root..i..addons_orchestrator.script_file)end
